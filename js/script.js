5d8687d1ed76b11390b7c16e981b7c6a4a025092
Montesi-Yuri <yuri13312@gmail.com>
2023-06-14 15:50:38 +0200

    Creata struttura js

--- js/script.js
+++ js/script.js
@@ -2,3 +2,33 @@
     JS
 */
 
+const ul = document.querySelector('.list');
+
+for (let numero = 1; numero <= 100; numero++) {
+
+    console.log('numeri fino a 100', numero);
+    const listItem = document.createElement('li');
+    listItem.append(numero);
+    ul.append(listItem);
+
+
+    if (numero % 3 == 0 && numero % 5 == 0) {
+        console.log(numero, 'è multiplo di 3 e di 5 (FIZZBUZZ)');
+        listItem.classList.add('multiplo_3_5')
+        
+    }
+
+    else if (numero % 5 == 0) {
+        console.log(numero, 'è multiplo di 5 (BUZZ)');
+        listItem.classList.add('multiplo_5')
+    }
+    
+    else if (numero % 3 == 0) {
+        console.log(numero, 'è multiplo di 3 (FIZZ)');
+        listItem.classList.add('multiplo_3')
+    }
+    
+}
+
+
+
