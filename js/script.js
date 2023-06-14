4f6cfa689c4e650dd6e10986d3466d9ca500afd8
Montesi-Yuri <yuri13312@gmail.com>
2023-06-14 16:10:51 +0200

    Aggiunto html e css

--- js/script.js
+++ js/script.js
@@ -10,22 +10,24 @@ for (let numero = 1; numero <= 100; numero++) {
     const listItem = document.createElement('li');
     listItem.append(numero);
     ul.append(listItem);
-
+    listItem.classList.add('multiplo_no')
 
     if (numero % 3 == 0 && numero % 5 == 0) {
         console.log(numero, 'è multiplo di 3 e di 5 (FIZZBUZZ)');
         listItem.classList.add('multiplo_3_5')
-        
+        listItem.innerHTML = 'FIZZBUZZ'
     }
 
     else if (numero % 5 == 0) {
         console.log(numero, 'è multiplo di 5 (BUZZ)');
         listItem.classList.add('multiplo_5')
+        listItem.innerHTML = 'BUZZ'
     }
     
     else if (numero % 3 == 0) {
         console.log(numero, 'è multiplo di 3 (FIZZ)');
         listItem.classList.add('multiplo_3')
+        listItem.innerHTML = 'FIZZ'
     }
     
 }
