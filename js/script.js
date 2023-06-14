521331792ef4bb3c4d26caea23828f99ed3935aa
Montesi-Yuri <yuri13312@gmail.com>
2023-06-14 16:14:08 +0200

    Completato esercizio

--- js/script.js
+++ js/script.js
@@ -11,22 +11,26 @@ for (let numero = 1; numero <= 100; numero++) {
     listItem.append(numero);
     ul.append(listItem);
     listItem.classList.add('multiplo_no')
+    listItem.classList.add('quadrato')
 
     if (numero % 3 == 0 && numero % 5 == 0) {
         console.log(numero, 'è multiplo di 3 e di 5 (FIZZBUZZ)');
         listItem.classList.add('multiplo_3_5')
+        listItem.classList.add('quadrato')
         listItem.innerHTML = 'FIZZBUZZ'
     }
 
     else if (numero % 5 == 0) {
         console.log(numero, 'è multiplo di 5 (BUZZ)');
         listItem.classList.add('multiplo_5')
+        listItem.classList.add('quadrato')
         listItem.innerHTML = 'BUZZ'
     }
     
     else if (numero % 3 == 0) {
         console.log(numero, 'è multiplo di 3 (FIZZ)');
         listItem.classList.add('multiplo_3')
+        listItem.classList.add('quadrato')
         listItem.innerHTML = 'FIZZ'
     }
     
